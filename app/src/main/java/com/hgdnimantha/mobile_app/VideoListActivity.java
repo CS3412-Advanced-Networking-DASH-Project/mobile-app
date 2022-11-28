3b97a604fe306bbcc8552d7e24b8317d7a36a454
dn00z <hgdnimantha@gmail.com>
2022-11-28 20:23:22 +0530

    added video list activity

---
+++ app/src/main/java/com/hgdnimantha/mobile_app/VideoListActivity.java
@@ -0,0 +1,17 @@
+package com.hgdnimantha.mobile_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class VideoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_list);
+    }
+}
